37cf36238b8b29bac6555b516d17a0e4e6db008e
PauXfighter <pauxfighter@gmail.com>
2026-03-26 10:50:25 +0100

    Paso dos

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,12 +18,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 1. Forzar la orientación apaisada
+        // Forzar la orientación apaisada
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        // 2. Configurar el TextView de mensajes para que tenga scroll
-        // (Asegúrate de que el ID coincida con el que le pongas en el XML, aquí uso 'textViewMissatges' de ejemplo)
-        TextView tvMissatges = findViewById(R.id.tituloJuego);
+        // Configurar el TextView de mensajes para que tenga scroll
+        TextView tvMissatges = findViewById(R.id.textViewMissatges);
         if (tvMissatges != null) {
             tvMissatges.setMovementMethod(new ScrollingMovementMethod());
         }
